04d2f6b74b6ee3e0611b1db56f671e71eb6e5f53
JoSaGuDu <joseguti5@gmail.com>
2018-07-20 17:31:16 -0400

    copy task added

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync');
+const autoPrefixer = require('gulp-autoprefixer');
+
+//Workflow variables
 const reload = browserSync.reload;
 const SOURCEPATHS = {
-  sassSource: 'src/scss/*.scss'
+  sassSource: 'src/scss/*.scss',
+  htmlSource: 'src/*.html'
 }
 const APPPATHS = {
   root: 'app/',
@@ -12,8 +16,10 @@ const APPPATHS = {
   js: 'app/js'
 }
 
+//Tasks
 gulp.task('sass', function () {
   return gulp.src(SOURCEPATHS.sassSource)
+    .pipe(autoPrefixer())
     .pipe(sass({ outputStyle: 'expanded' })
       .on('error', sass.logError))
     .pipe(gulp.dest(APPPATHS.css));
@@ -27,7 +33,12 @@ gulp.task('serve', function () {
   });
 });
 
-gulp.task('watch', ['sass', 'serve'], function () {
+gulp.task('copy', function () {
+  gulp.src(SOURCEPATHS.htmlSource)
+  .pipe(gulp.dest(APPPATHS.root));
+});
+
+gulp.task('watch', ['sass', 'serve', 'copy'], function () {
   gulp.watch([SOURCEPATHS.sassSource], ['sass']);
 });
 
